80aa982295f03e3309e2dc5dd2454c97c646974d
Rafal <les92raf@gmail.com>
2019-08-16 20:56:55 +0200

    Initial commit

---
+++ app/src/main/java/com/rl/dynamicrates/RatesActivity.kt
@@ -0,0 +1,12 @@
+package com.rl.dynamicrates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RatesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rates)
+    }
+}
